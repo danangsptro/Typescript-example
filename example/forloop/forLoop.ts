7683d9fe99328db4af0bbd9e6e5de61c8f8f2eff
danangsptro <66934332+danangsptro@users.noreply.github.com>
2022-11-27 23:15:06 +0700

    Update forLoop.ts

--- example/forloop/forLoop.ts
+++ example/forloop/forLoop.ts
@@ -1,6 +1,6 @@
 let result: number = 1; // First, define the default number  for number list which showing
 const input: number = 15; // input value
-let tx: string[] = ['Fintegra', 'Homido Indonesia'] // Text which show 
+let tx: string[] = ['Tes1', 'Test2'] // Text which show 
 
 for (result; result <= input; result++) {
     if (result% 3 === 0 && result% 5 == 0) {
